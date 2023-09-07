6ddfc187b5132b5afd8bb33f2f398cb7b22d9339
Sabina Kromraj <sabina.kromraj@gmail.com>
2023-09-07 14:19:04 +0200

    remove class MediaLibrary, fix get_movies/get_series, add run_generate_views

--- media_library.py
+++ media_library.py
@@ -23,71 +23,53 @@ class Series(Movie):
     def __str__(self):
         return f"{self.title} S{self.season:02}E{self.episode:02}"
     
-class MediaLibrary:
-    def __init__(self):
-        self.media_collection = []
+def get_movies(media_library):
+    movies = [str(media) for media in media_library if isinstance(media, Movie) and not isinstance(media, Series)]
+    return sorted(movies)
+
+def get_series(media_library):
+    series = [str(media) for media in media_library if isinstance(media, Series)]
+    return sorted(series)
     
-    def add_media(self, media):
-        self.media_collection.append(media)
-
-    def play_media(self, title):
-        for media in self.media_collection:
-            if media.title == title:
-                media.play()
-
-    def get_movies(library):
-        movies = [media for media in library.media_collection if isinstance(media, Movie)]
-        movies.sort(key=lambda x: x.title)
-        return movies
-
-    def get_series(library):
-        series = [media for media in library.media_collection if isinstance(media, Series)]
-        series.sort(key=lambda x: x.title)
-        return series  
+def search(media_library, query):
+    results = [media for media in media_library if query in media.title]
+    return results
     
-    def search(library, query):
-        results = [media for media in library.media_collection if query in media.title]
-        return results
-    
-    def generate_views(library):
-        for i in range(10):
-            media = random.choice(library.media_collection)
-            views = random.randint(1, 100)
-            media.views += views
+def generate_views(media_library):
+    media = random.choice(media_library)
+    views = random.randint(1, 100)
+    media.views += views
 
-    def get_top_titles(self, count):
-        sorted_media = sorted(self.media_collection, key=lambda x: x.views, reverse=True)
-        return sorted_media[:count]
+def run_generate_views(media_library, num_runs=10):
+    for i in range(num_runs):
+        generate_views(media_library)
+
+def get_top_titles(media_library, count):
+    sorted_media = sorted(media_library, key=lambda x: x.views, reverse=True)
+    return sorted_media[:count]
 
 
 if __name__ == "__main__":
     print("Biblioteka filmów")
                 
-    library = MediaLibrary()
+media_library = [
+    Movie("Pulp Fiction", 1994, "Crime"),
+    Movie("The Shawshank Redemption", 1994, "Drama"),
+    Movie("The Green Mile", 1999, "Drama"),
+    Movie("Forrest Gump", 1994, "Drama"),
+    Series("Breaking Bad", 2008, "Crime", 1, 1),
+    Series("Game of Thrones", 2011, "Fantasy", 2, 2),
+    Series("House M.D.", 2004, "Drama/Comedy", 3, 3),
+    Series("Stranger Things", 2016, "Drama/Horror/Sci-Fi", 1, 1)
+]
 
-    movie1 = Movie("Pulp Fiction", 1994, "Crime")
-    movie2 = Movie("The Shawshank Redemption", 1994, "Drama")
-    movie3 = Movie("The Green Mile", 1999, "Drama")
-    movie4 = Movie("Forrest Gump", 1994, "Drama")
-    series1 = Series("Breaking Bad", 2008, "Crime", 1, 1)
-    series2 = Series("Game of Thrones", 2011, "Fantasy", 2, 2)
-    series3 = Series("House M.D.", 2004, "Drama/Comedy", 3, 3)
-    series4 = Series("Stranger Things", 2016, "Drama/Horror/Sci-Fi", 1, 1)
+run_generate_views(media_library, 10)
 
-    library.add_media(movie1)
-    library.add_media(movie2)
-    library.add_media(movie3)
-    library.add_media(movie4)
-    library.add_media(series1)
-    library.add_media(series2)
-    library.add_media(series3)
-    library.add_media(series4)
+current_date = datetime.now().strftime("%d.%m.%Y")
+print(f"Najpopularniejsze filmy i seriale dnia {current_date}:")
 
-    library.generate_views()
+top_titles = get_top_titles(media_library, 3)
+for idx, title in enumerate(top_titles, start=1):
+    print(f"{idx}. {title} ({title.views} odtworzeń)")
 
-    current_date = datetime.now().strftime("%d.%m.%Y")
-    print(f"Najpopularniejsze filmy i seriale dnia {current_date}:")
 
-    top_titles = library.get_top_titles(3)
-    for idx, title in enumerate(top_titles, start=1):
-        print(f"{idx}. {title} ({title.views} odtworzeń)")
